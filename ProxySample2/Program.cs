f62a55abb9c2faea8e1ba79f62ed8edff3541130
Clay Lenhart <clay@lenharts.net>
2023-01-01 16:09:48 -0500

    new dotnet 7 rate limiter

---
+++ ProxySample2/Program.cs
@@ -0,0 +1,38 @@
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+var concurrencyPolicy = "Concurrency";
+builder.Services.AddRateLimiter(o =>
+{
+    o.AddConcurrencyLimiter(policyName: concurrencyPolicy, options =>
+    {
+        options.PermitLimit = 50;
+        options.QueueProcessingOrder = QueueProcessingOrder.NewestFirst;
+        options.QueueLimit = 0;
+    });
+
+    o.OnRejected = async (context, token) =>
+    {
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        context.HttpContext.Response.ContentType = "text/html";
+        await context.HttpContext.Response.WriteAsync("<html><body><h1>rejected</h1></body></html>");
+    };
+
+});
+
+var app = builder.Build();
+
+app.UseRateLimiter();
+app.MapGet("/", async () =>
+{
+    await Task.Delay(50);
+    return "Hello World!";
+}).RequireRateLimiting(concurrencyPolicy);
+
+
+
+app.Run();
